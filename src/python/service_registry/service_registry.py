cdfb83f64b50571aa20a7309c10034be0378f570
simoneb <simonebauco1@gmail.com>
2023-10-13 11:54:41 +0200

    Implemented service registry

---
+++ src/python/service_registry/service_registry.py
@@ -0,0 +1,23 @@
+import json
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/service-registry', methods=['GET'])
+def request_handler():
+    country = request.args.get('country')
+    with open('routing.json', 'r') as f:
+        routing_data = json.load(f)
+    try :
+        port_number = routing_data[country]
+        return_data = {'port_number': port_number}
+    except KeyError:
+        return json.dumps("This country is not supported"), 404
+
+    return json.dumps(return_data), 200
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=9000)
